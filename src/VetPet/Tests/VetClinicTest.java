01a2314ecd3e08b1c0a2ed568b49201ef5559004
Mark Schoendorfer <mark.schoendorfer@ucalgary.ca>
2019-10-11 22:53:56 -0600

    Added additional tests for VetClinic

--- src/VetPet/Tests/VetClinicTest.java
+++ src/VetPet/Tests/VetClinicTest.java
@@ -25,9 +25,10 @@ public class VetClinicTest {
 	public void test() {
 		vc.addCat("Oliver", "000001", "Mouse", "Active");
 		assertEquals(1, vc.numAnimals());
+		assertEquals("Animal: Cats\n\t ID: 000001 Name: Oliver Type: Active Toy: Mouse\n\nAnimal: Dogs\n", vc.listAll_animals());
 		vc.addDog("Spot", "100000", "Collie", "Active");
 		assertEquals(2, vc.numAnimals());
-		
+		assertEquals("(DOG) ID: 100000 Name: Spot Type: Active K9_family: Collie\n", vc.getAnimalInformation("100000"));
 	}
 
 }
